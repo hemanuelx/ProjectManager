8226e365905552e2ace88b0e955fbe4c1e4d882e
hemanuelx <hemanuel.xafranski@misterspex.de>
2022-04-09 11:10:34 +0200

    remove unused import

--- src/main/java/br/com/projectmanager/ProjectManager/adapters/UserController.java
+++ src/main/java/br/com/projectmanager/ProjectManager/adapters/UserController.java
@@ -3,7 +3,6 @@ package br.com.projectmanager.ProjectManager.adapters;
 import br.com.projectmanager.ProjectManager.application.user.UserUseCase;
 import br.com.projectmanager.ProjectManager.domain.user.User;
 import br.com.projectmanager.ProjectManager.domain.user.UserRepository;
-import br.com.projectmanager.ProjectManager.application.user.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
